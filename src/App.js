56ac46bd64109101d074c974b08913de8d160932
Michael <michaelwkaiser@gmail.com>
2017-12-06 18:54:34 -0500

    adds edit and create functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}/>
         <Resource name="users" list={UserList} />
     </Admin>
 );
